289c0178c5c99aac91860332f504317da376f1d4
Ziii2005 <trandaividt5@gmail.com>
2026-05-24 22:54:14 +0700

    Create gallery.js

---
+++ gallery.js
@@ -0,0 +1,11 @@
+function upDate(previewPic) {
+  console.log("Image source: " + previewPic.src);
+  console.log("Image alt: " + previewPic.alt);
+  document.getElementById('image').innerHTML = previewPic.alt;
+  document.getElementById('image').style.backgroundImage = "url('" + previewPic.src + "')";
+}
+
+function unDo() {
+  document.getElementById('image').style.backgroundImage = "url('')";
+  document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+}
