ad00280d20350b1660856c4c95c7f014f4ee54cc
Ziii2005 <trandaividt5@gmail.com>
2026-05-25 07:21:09 +0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,11 +1,15 @@
 function upDate(previewPic) {
-  console.log("Image source: " + previewPic.src);
-  console.log("Image alt: " + previewPic.alt);
+  /* Thay đổi text thành thuộc tính alt của ảnh nhỏ */
   document.getElementById('image').innerHTML = previewPic.alt;
+  
+  /* Thay đổi background-image thành ảnh nhỏ */
   document.getElementById('image').style.backgroundImage = "url('" + previewPic.src + "')";
 }
 
 function unDo() {
+  /* Xóa background-image */
   document.getElementById('image').style.backgroundImage = "url('')";
+  
+  /* Trả lại text ban đầu */
   document.getElementById('image').innerHTML = "Hover over an image below to display here.";
 }
